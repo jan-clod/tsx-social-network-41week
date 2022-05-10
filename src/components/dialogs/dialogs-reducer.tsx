a1e4ad1a6aa7a7875e8a3294fde1fa2ff037f701
JanClod <fia.yan@mail.ru>
2022-05-10 18:35:14 +0300

    qwe

--- src/components/dialogs/dialogs-reducer.tsx
+++ src/components/dialogs/dialogs-reducer.tsx
@@ -4,15 +4,9 @@ import { ActionTypes, dialogsPageType } from '../../redux/state';
 let initialState = {
     dialogsData: [
         { id: v1(), name: "Saha" },
-        { id: v1(), name: "Gena" },
         { id: v1(), name: "Anana" },
-        { id: v1(), name: "Miroslav" },
         { id: v1(), name: "Krocop" },
-        { id: v1(), name: "Ira" },
-        { id: v1(), name: "Mikle" },
         { id: v1(), name: "Gena L" },
-        { id: v1(), name: "Mikle" },
-        { id: v1(), name: "Mikki" },
         { id: v1(), name: "Outback" },
         { id: v1(), name: "Arny" },
         { id: v1(), name: "Crish" },
