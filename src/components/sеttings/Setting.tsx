3c3c3c5b78561687d4d5361bdd7180524093cec7
jan-clod <paviliong6tim@gmail.com>
2023-03-09 19:57:03 +0300

    white_tema

--- src/components/sеttings/Setting.tsx
+++ src/components/sеttings/Setting.tsx
@@ -4,7 +4,7 @@ import s from "./setting.module.css"
 export const Setting = () => {
     return(
         <div className={s.settingBlock}>
-            <Login/>
+            
         </div>
     )
 }
